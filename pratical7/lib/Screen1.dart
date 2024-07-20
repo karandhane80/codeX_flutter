fbf835b48dccda97c637c684fa1ecd38e1061455
unknown <karandhane80@gmail.com>
2024-07-20 23:39:54 +0530

    pratical7

--- pratical7/lib/Screen1.dart
+++ pratical7/lib/Screen1.dart
@@ -104,22 +104,19 @@ class Screen1 extends StatelessWidget {
              
                children: [
                  Container(
-                  width: 80,
-                  height: 80,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(10),
+                  width: 50,
+                  height: 50,
+                  margin: EdgeInsets.all(7),
+                  padding: EdgeInsets.all(7),
                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(60),image: DecorationImage(image: NetworkImage("https://media.gettyimages.com/id/156690524/photo/politician-balasaheb-thackeray-dies-aged-86.jpg?s=612x612&w=gi&k=20&c=u0jeafy5KHh7rPqBGW2Af5dWDvQZ6z6Wsp8gguni5fs="),
                   fit: BoxFit.cover),boxShadow: [BoxShadow(spreadRadius: 0.6,blurRadius: 0.3,color: Colors.black)]),
-                  
+                 ),Container(
+                  margin: EdgeInsets.all(7),
+               child: Text("fan-mv",style: TextStyle(color: const Color.fromARGB(255, 248, 1, 1),fontSize: 20)),
                  ),
-                 Padding(padding: EdgeInsets.all(100)),
-                 Icon(Icons.drag_indicator_sharp,color: Colors.white,)
-
-                 
-            
-                 
-                 
-               ],
+                 Padding(padding: EdgeInsets.all(110)),
+                 Icon(Icons.more_vert,color: Colors.white,)
+             ],
              ) ,   ),
                 Row(
                   children: [
@@ -133,24 +130,38 @@ class Screen1 extends StatelessWidget {
                     SizedBox(
                       width: 260,
                     ),
-                 
             Icon(
               Icons.bookmark,
             ),
-            
-           ],
-                   
-                   
-                  
+           ], 
                 ),
-       
             Container(
                   margin: EdgeInsets.only(top: 15,bottom: 15),
                   height: 400,
                   width: 400,
                   decoration: BoxDecoration(image: DecorationImage(image: NetworkImage("https://media.gettyimages.com/id/1241052221/photo/bollywood-actor-shahid-kapoor-arrives-to-attend-a-press-conference-ahead-of-22nd-edition-of.jpg?s=612x612&w=gi&k=20&c=VoSyQzgxRQooHT1HZSkeStsv1-0MyQB1Z1EK5ySAT-0="),
-                  fit: BoxFit.cover),border: Border.all(color: Colors.black12,width: 0.5))
-                ),
+                  fit: BoxFit.cover),border: Border.all(color: Colors.black12,width: 0.5)),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.start,
+             crossAxisAlignment: CrossAxisAlignment.start,
+             
+               children: [
+                 Container(
+                  width: 50,
+                  height: 50,
+                  margin: EdgeInsets.all(7),
+                  padding: EdgeInsets.all(7),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(60),image: DecorationImage(image: NetworkImage("https://media.gettyimages.com/id/156690524/photo/politician-balasaheb-thackeray-dies-aged-86.jpg?s=612x612&w=gi&k=20&c=u0jeafy5KHh7rPqBGW2Af5dWDvQZ6z6Wsp8gguni5fs="),
+                  fit: BoxFit.cover),boxShadow: [BoxShadow(spreadRadius: 0.6,blurRadius: 0.3,color: Colors.black)]),
+                 ),Container(
+                  margin: EdgeInsets.all(7),
+               child: Text("fan-mv",style: TextStyle(color: const Color.fromARGB(255, 248, 1, 1),fontSize: 20)),
+                 ),
+                 Padding(padding: EdgeInsets.all(110)),
+                 Icon(Icons.more_vert,color: Colors.white,)
+             ],
+             ) ,   ),
+  
+             
                 Row(
                   children: [
                     Icon(Icons.favorite,),SizedBox(
@@ -162,24 +173,40 @@ class Screen1 extends StatelessWidget {
                     Icon(Icons.screen_share_rounded),
                     SizedBox(
                       width: 260,
-                    ),
-                 
+                    ),  
             Icon(
               Icons.bookmark,
             ),
             
            ],
-                   
-                   
-                  
-                ),
+                    ),
                  Container(
                   margin: EdgeInsets.only(top: 15,bottom: 15),
                   height: 400,
                   width: 400,
                   decoration: BoxDecoration(image: DecorationImage(image: NetworkImage("https://media.gettyimages.com/id/1241052221/photo/bollywood-actor-shahid-kapoor-arrives-to-attend-a-press-conference-ahead-of-22nd-edition-of.jpg?s=612x612&w=gi&k=20&c=VoSyQzgxRQooHT1HZSkeStsv1-0MyQB1Z1EK5ySAT-0="),
-                  fit: BoxFit.cover),border: Border.all(color: Colors.black12,width: 0.5))
-                ),
+                  fit: BoxFit.cover),border: Border.all(color: Colors.black12,width: 0.5)),
+             child: Row(mainAxisAlignment: MainAxisAlignment.start,
+             crossAxisAlignment: CrossAxisAlignment.start,
+             
+               children: [
+                 Container(
+                  width: 50,
+                  height: 50,
+                  margin: EdgeInsets.all(7),
+                  padding: EdgeInsets.all(7),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(60),image: DecorationImage(image: NetworkImage("https://media.gettyimages.com/id/156690524/photo/politician-balasaheb-thackeray-dies-aged-86.jpg?s=612x612&w=gi&k=20&c=u0jeafy5KHh7rPqBGW2Af5dWDvQZ6z6Wsp8gguni5fs="),
+                  fit: BoxFit.cover),boxShadow: [BoxShadow(spreadRadius: 0.6,blurRadius: 0.3,color: Colors.black)]),
+                 ),Container(
+                  margin: EdgeInsets.all(7),
+               child: Text("fan-mv",style: TextStyle(color: const Color.fromARGB(255, 248, 1, 1),fontSize: 20)),
+                 ),
+                 Padding(padding: EdgeInsets.all(110)),
+                 Icon(Icons.more_vert,color: Colors.white,)
+             ],
+             ) ,   ),
+  
+                
                 Row(
                   children: [
                     Icon(Icons.favorite,),SizedBox(
@@ -198,9 +225,7 @@ class Screen1 extends StatelessWidget {
             ),
             
            ],
-                   
-                   
-                  
+    
                 ),
               ],
              )
